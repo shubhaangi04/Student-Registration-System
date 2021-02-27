b2a63acb387c11a502bcb801792627ffbf6a1f1b
Vikas <vikas.kumar@target.com>
2021-02-27 20:34:33 +0530

    Reverted changes

--- src/main/java/com/assignment/registration/StudentRegistrationSystem/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.assignment.registration.StudentRegistrationSystem.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.assignment.registration.StudentRegistrationSystem.model.Student;
-
-public interface StudentRepository extends CrudRepository<Student, Integer>{
-	
-}
